431dafcaa8b1cf41905b8efe6f8ba5686bd75cff
Hsuan Huang <67884719+hsuan0320@users.noreply.github.com>
2023-09-10 00:31:08 +0800

    完成首頁及常見問題

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,22 +1,37 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    speed: 1000,
-     autoplay: {
-       delay: 3000,
-       disableOnInteraction: false,
-     },
-      breakpoints: {
-        768: {
-          autoplay: {
-              enabled: true, //設定為false 為768px 以上時，放棄所有功能；設定為true 為768px 以上時，可開啟下列設定
-              delay: 1000,
-            }
-        }
-      },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const aboutSwiper = new Swiper('.aboutSwiper', {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      autoplay: {
+        enabled: true, //設定為false 為768px 以上時，放棄所有功能；設定為true 為768px 以上時，可開啟下列設定
+        delay: 1000,
+      }
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const shareSwiper = new Swiper(".share__swiper", {
+  // slide 間距
+  spaceBetween: 24,
+  slidesPerView: 3,
+  // Scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+
+    //需增加 draggable 可拉動下方滾動條
+    draggable: true
+  },
+  // 使 swiper 可以使用滑鼠滾輪捲動
+  mousewheel: true
+});
--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import '@popperjs/core/dist/umd/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import AOS from 'aos';
 import './assets/js/swiper';
 import './assets/scss/all.scss';
 
-
 console.log("Hello world!");
+AOS.init({
+    easing: "ease", // AOS 動畫的默認緩動
+    once: true, // 是否動畫應該只發生一次 - 向下滾動時
+    duration: 800
+});
